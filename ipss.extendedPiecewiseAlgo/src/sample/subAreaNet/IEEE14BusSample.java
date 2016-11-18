16c49d916e3f4c79bab333d30a2de323082a3853
jinjingfarm <mzhou@interpss.org>
2016-11-18 23:49:02 +0800

    added subAreaNet.IEEE14BusSample sample.

---
+++ ipss.extendedPiecewiseAlgo/src/sample/subAreaNet/IEEE14BusSample.java
@@ -0,0 +1,98 @@
+ /*
+  * @(#)IEEE14BusSample.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 04/15/2016
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package sample.subAreaNet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.math3.complex.Complex;
+import org.interpss.CorePluginFactory;
+import org.interpss.IpssCorePlugin;
+import org.interpss.fadapter.IpssFileAdapter;
+import org.interpss.piecewise.base.SubAreaNetProcessor;
+import org.interpss.piecewise.onephase.CuttingBranch1Phase;
+import org.interpss.piecewise.onephase.SubArea1Phase;
+import org.interpss.piecewise.onephase.SubNetwork1Phase;
+import org.interpss.piecewise.onephase.impl.SubArea1PhaseProcessorImpl;
+import org.interpss.piecewise.onephase.impl.SubNet1PhaseProcessorImpl;
+
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.aclf.AclfBranch;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+
+public class IEEE14BusSample {
+	
+	public static void main(String args[]) throws Exception {
+		// initialize InterPSS plugin
+		IpssCorePlugin.init();
+		
+		System.out.println("======================SubArea processing ==================");
+		subAreaProcessing();
+		
+		System.out.println("======================SubNetwork processing ==================");
+		subNetworkProcessing();	
+		
+	}
+	
+	static void subAreaProcessing() throws InterpssException {
+		AclfNetwork net = CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IpssInternal)
+				.load("testdata/ieee14.ipssdat")
+				.getAclfNet();
+				
+		SubAreaNetProcessor<AclfBus, AclfBranch, SubArea1Phase, Complex> 
+			proc = new SubArea1PhaseProcessorImpl<>(net, new CuttingBranch1Phase[] { 
+						new CuttingBranch1Phase("4->71(1)"),
+						new CuttingBranch1Phase("4->91(1)"),
+						new CuttingBranch1Phase("5->61(1)")});	
+		
+		proc.processSubAreaNet();
+		
+		proc.getSubAreaNetList().forEach(subArea -> {
+			System.out.println("----------------------");
+			System.out.println(subArea);
+		});		
+	}
+	
+	static void subNetworkProcessing() throws InterpssException {
+		AclfNetwork net = CorePluginFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.IpssInternal)
+				.load("testdata/ieee14.ipssdat")
+				.getAclfNet();		
+		
+		SubAreaNetProcessor<AclfBus, AclfBranch, SubNetwork1Phase, Complex> proc = new SubNet1PhaseProcessorImpl(net, new CuttingBranch1Phase[] { 
+				new CuttingBranch1Phase("4->71(1)"),
+				new CuttingBranch1Phase("4->91(1)"),
+				new CuttingBranch1Phase("5->61(1)")});	
+		
+		proc.processSubAreaNet();
+		
+		proc.getSubAreaNetList().forEach(subNet -> {
+			System.out.println("----------------------");
+			System.out.println(subNet);
+		});		
+	}
+	
+}
